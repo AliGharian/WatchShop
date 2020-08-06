b814b81d52611434bd2589021c47537e85308d86
AliGharian <ali.gharian98@gmail.com>
2020-08-06 12:06:31 +0430

    add some color styles

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+//colors
+const Color kDarkBrown = Color(0xFFAA402E);
+const Color kLightBrown = Color(0xFFD9996C);
+const Color kLightBlack = Color(0xFF65534E);
+const Color kLightGrey = Color(0xFFC5C5C5);
+const Color kDarkGrey = Color(0xFF9D918D);
